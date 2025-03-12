4b3cae8dd211ce0c6ebc755bed70328bd29e7d47
istvan-knab <istvan.knab@icloud.com>
2025-03-12 18:06:27 +0100

    test response

--- llm_factory.py
+++ llm_factory.py
@@ -2,7 +2,15 @@ import ollama
 
 class LLM:
 
-    def __init__(self, model:str) -> None:
-        client = ollama.Client()
-        response = client.generate(model=model, prompt='Who are you? which model are you?')
+    def __init__(self, model:str, env) -> None:
+        self.model = model
+        self.client = ollama.Client()
+        response = self.client.generate(model=model, prompt=f'Please consider the problem of the environment{env}, '
+                                                            f'you will have to find the best way, the url of description'
+                                                            f' is the following:https://gymnasium.farama.org/environments/toy_text/frozen_lake/')
         print(response['response'])
+
+    def step(self, state):
+        prompt='You have to use a valid action from the action space. I need a single integer in the response'
+        action = self.client.generate(model=self.model,prompt=prompt)
+        return action['response']
--- run_llm.py
+++ run_llm.py
@@ -1,7 +1,19 @@
 import json
 from llm_factory import LLM
+import gymnasium as gym
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
 if __name__ == "__main__":
     with open('config.json', 'r') as f:
         config = json.load(f)
-    model = LLM(config['model'])
+
+    env = gym.make('FrozenLake-v1', desc=generate_random_map(size=8))
+    model = LLM(config['model'], env)
+
+
+    state = env.reset()
+    done = False
+    while not done:
+        action = model.step(state)
+        state, reward, terminated, truncated, info = env.step(action)
+
